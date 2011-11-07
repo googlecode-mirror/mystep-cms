fd234e9ebb1b3bfc2dfefc9cd110fc401e2f61e1
windy2006 <windy2006@gmail.com>
2011-11-07 08:54:57 +0000

    Fix a problem in update record...

--- update/index.php
+++ update/index.php
@@ -8,7 +8,7 @@ require(ROOT_PATH."/source/class/mydb.class.php");
 require("version.php");
 
 $v = $_GET['v'];
-if($v!="" && !empty($_Server["HTTP_REFERER"])) {
+if($v!="") {
 	$cache_file = ROOT_PATH."/".$setting['path']['cache']."/update/".md5($v.$ms_version['ver']);
 	if(file_exists($cache_file)) {
 		$update = GetFile($cache_file);
