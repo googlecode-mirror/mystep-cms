849ac155c9c94c7e65a9a91c8bd564ad3d6db21c
windy2006 <windy2006@gmail.com>
2012-02-13 02:12:54 +0000

    Some adjusts...

--- plugin/email/config.php
+++ plugin/email/config.php
@@ -3,8 +3,8 @@ $plugin_setting['email'] = array();
 $plugin_setting['email']['mode'] = 'ssl';
 $plugin_setting['email']['host'] = 'smtp.gmail.com';
 $plugin_setting['email']['port'] = 465;
-$plugin_setting['email']['user'] = 'windy2006';
-$plugin_setting['email']['password'] = 'mygod2000';
+$plugin_setting['email']['user'] = '';
+$plugin_setting['email']['password'] = '';
 
 
 ?>
